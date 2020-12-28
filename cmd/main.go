e16f09a3f196e41bb4256d2d117e32a272faf06a
rudras-hub <suchitra.rudra@netapp.com>
2020-12-28 13:27:29 -0800

    Adding some rough code to main.go and ignoring the executable, for now.

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,19 @@
-package cmd
+package main
+
+import (
+	"andaz-api/internal/data/handler"
+	"fmt"
+)
+
+func main(){
+	c := handler.GetCharacterRepositoryInstance()
+	characterEntities, err := c.RetrieveAll()
+	if err != nil{
+		fmt.Println("Error = ", err)
+		return
+	}
+
+	for _, char := range  characterEntities{
+		fmt.Println("Name = ", char.Name, " Alias = ", char.Alias)
+	}
+}
